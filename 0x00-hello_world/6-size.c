22d4a83cd25c3c11b3918cb397ccbf2ce321b94a
Ahmedadel200066 <medo.adel200@gmail.com>
2023-03-16 15:27:02 -0700

    z

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-	print("size of a char: %lu byte(s)\n", sizeof(char));
-	print("size of an int: %lu byte(s)\n", sizeof(int));
-	print("size of a long int: %lu byte(s)\n", sizeof(long int));
-	print("size of a long long int: %lu byte(s)\n", sizeof(long long int));
-	print("size of a float: %lu byte(s)\n", sizeof(float));
+	print("size of a char: %u byte(s)\n", sizeof(char));
+	print("size of an int: %u byte(s)\n", sizeof(int));
+	print("size of a long int: %u byte(s)\n", sizeof(long int));
+	print("size of a long long int: %u byte(s)\n", sizeof(long long int));
+	print("size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
