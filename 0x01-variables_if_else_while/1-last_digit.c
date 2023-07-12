be45b38e19355a4730baaf8e681d8b7b27ffe854
Ahmedadel200066 <medo.adel200@gmail.com>
2023-07-12 03:52:37 -0700

    z

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
         {
                 printf("%i and is 0\n", n);
         }
-	else (n < 6)
+	if else (n < 6)
         {
                 printf("%i and is less than 6 and not 0\n", n);
         }
