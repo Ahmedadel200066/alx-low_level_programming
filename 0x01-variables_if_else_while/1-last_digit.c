45e5a4b8f65834ebd486cd58b9bb1ba2439b9e8b
Ahmedadel200066 <medo.adel200@gmail.com>
2023-07-12 04:04:41 -0700

    z

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,7 +25,7 @@ int main(void)
 	}
 	else if (n < 6)
 	{
-		printf("%i last digit and is less than 6 and not 0\n", n)
+		printf("%i last digit and is less than 6 and not 0\n", n);
 	}
 	return (0);
 }
