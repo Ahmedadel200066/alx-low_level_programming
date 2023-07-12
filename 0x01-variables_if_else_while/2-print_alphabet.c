9e925a68962a166fb65c31a4a13757250272fe1f
Ahmedadel200066 <medo.adel200@gmail.com>
2023-07-12 05:12:27 -0700

    z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - zby
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char x;
+	int i;
+
+	for (i = 97 ; i >= 122 ; i++)
+	{
+		char x = 'i';
+
+		putchar(x);
+	}
+	return (0);
+}
