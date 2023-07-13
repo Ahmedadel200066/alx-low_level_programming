e0633f0aef9f8c4135496ec30d8adc71dbd05d78
Ahmedadel200066 <medo.adel200@gmail.com>
2023-07-13 16:53:38 -0700

    z

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - zby
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		if (i == 'e' && i == 'q')
+		{
+			i++;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
