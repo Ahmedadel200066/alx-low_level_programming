df4f2f945cc11d5e26d058b2dfcb21b95f700b60
Ahmedadel200066 <medo.adel200@gmail.com>
2023-07-13 16:42:52 -0700

    z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - zby
+ *
+ * Return:0
+ *
+ */
+int main(void)
+{
+	char i;
+	char x;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	for (x = 'A' ; x <= 'Z' ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
