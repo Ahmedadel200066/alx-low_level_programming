2cb96b8b017329549146039247dfa5513c0a4c8b
Ahmedadel200066 <medo.adel200@gmail.com>
2023-07-13 18:32:23 -0700

    z

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,22 +9,22 @@
 
 int main(void)
 {
-	int i,x;
+	int i, x;
 	int comma = 44;
 	int space = 32;
 
 	for (x = 48 ; x <= 57 ; x++)
 	{
-		for (i = 48 ; i <=57 ; i++)
+		for (i = 49 ; i <= 57 ; i++)
 		{
 		putchar(x);
 		putchar(i);
+		putchar(comma);
+		putchar(space);
 		if (x == i)
 		{
 			continue;
 		}
-		putchar(comma);
-		putchar(space);
 		}
 	}
 	putchar('\n');
