2b0e4682ef84aadfd61af6f7f24953ea6c8a6575
Ahmedadel200066 <medo.adel200@gmail.com>
2023-07-13 17:10:00 -0700

    z

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include  <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:0
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+		putchar('x');
+	}
+	putchar('\n');
+
+	return (0);
+}
