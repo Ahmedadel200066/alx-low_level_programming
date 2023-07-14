567b96f979c53983053a2f2681825fc777655c1e
Ahmedadel200066 <medo.adel200@gmail.com>
2023-07-14 05:53:33 -0700

    z

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 			{
 				for (d = 48; d <= 57; d++)
 				{
-				if (((c + d) > (a + b) && c >= a) || a < c)
+				if ((c + d) > (a + b) && c >= a)
 				{
 					putchar(a);
 					putchar(b);
