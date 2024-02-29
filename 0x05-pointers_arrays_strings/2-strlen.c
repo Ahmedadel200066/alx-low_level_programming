db5f30f1dddcd3bc56900af8f8f7a16541eeda5f
Ahmedadel200066 <medo.adel200@gmail.com>
2024-02-29 10:00:20 +0200

    Update parameter name in _strlen function

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
 * _strlen - Returns the length of a string
-* @str: The string to get the lenght of
+* @s: The string to get the lenght of
 * Return: The lenght of @str
 */
-size_t _strlen(const char *str)
+int _strlen(char *s)
 {
-	size_t length = 0;
+	int x = 0;
 
-	while (*str++)
-	length++;
-	return (length);
+	while (*s++)
+	x++;
+	return (x);
 }
