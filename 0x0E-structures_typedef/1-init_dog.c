a5b9a53148abd406787f41bfa8f1735269db84cc
Ahmedadel200066 <medo.adel200@gmail.com>
2024-09-10 02:45:39 +0300

    Initialize dog structure with provided values

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,18 @@
 #include "dog.h"
+/**
+ * init_dog - Initializes a dog structure with the given values.
+ * @d: Pointer to the dog structure to be initialized.
+ * @name: Pointer to the name of the dog.
+ * @age: Age of the dog.
+ * @owner: Pointer to the owner's name of the dog.
+ *
+ * Description: This function initializes a dog structure with the provided
+ *              values for name, age, and owner.
+ */
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    d->age = age;
+    d->name = name;
+    d->owner = owner;
+}
