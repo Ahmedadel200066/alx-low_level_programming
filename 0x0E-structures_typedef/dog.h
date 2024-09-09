d21693e3ad7c74757c0db6ae850c2cc75c2e6307
Ahmedadel200066 <medo.adel200@gmail.com>
2024-09-10 02:18:58 +0300

    Update dog.h with improved struct definition for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog
 /**
- * @struct : about dog stats
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
  *
- */
+ **/
+struct dog
 {
 	char *name;
 	float age;
