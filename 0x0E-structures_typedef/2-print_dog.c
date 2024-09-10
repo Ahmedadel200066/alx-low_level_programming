c058d1540bf45ba9a2dcbd3f95f2ef76af81c947
Ahmedadel200066 <medo.adel200@gmail.com>
2024-09-10 19:41:30 +0300

    Refactor print_dog function to handle NULL values

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,26 +1,34 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+ * print_dog - Prints the details of a dog structure.
+ * @d: Pointer to the struct dog to be printed.
+ *
+ * Description: This function prints the name, age, and owner of a dog.
+ * If an element of d is NULL, it prints (nil) instead of the element.
+ * If d is NULL, it does nothing.
+ */
 void print_dog(struct dog *d)
 {
-    if (d != NULL)
-    {
-        if (d->name != NULL)
-        {
-            printf("%s\n", d->name);
-        }
-        else if (d->name == NULL)
-        {
-            printf("nil\n");
-        }
-        printf("%f\n", d->age);
-        if (d->owner != NULL)
-        {
-            printf("%s\n", d->owner);
-        }
-        else if (d->owner == NULL)
-        {
-            printf("nil\n");
-        }
-    }
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			printf("%s\n", d->name);
+		}
+		else if (d->name == NULL)
+		{
+			printf("nil\n");
+		}
+		printf("%f\n", d->age);
+		if (d->owner != NULL)
+		{
+			printf("%s\n", d->owner);
+		}
+		else if (d->owner == NULL)
+		{
+			printf("nil\n");
+		}
+	}
 }
